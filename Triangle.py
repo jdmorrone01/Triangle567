d9330a541dc3fcaf0efb847ad116cba691bc8160
jdmorrone01 <74426727+jdmorrone01@users.noreply.github.com>
2021-10-09 16:10:55 -0400

    got rid of blank space

--- Triangle.py
+++ Triangle.py
@@ -13,17 +13,14 @@ def classifyTriangle(a,b,c):
     """
     Your correct code goes here...  Fix the faulty logic below until the code passes all of 
     you test cases. 
-    
     This function returns a string with the type of triangle from three integer values
     corresponding to the lengths of the three sides of the Triangle.
-    
     return:
         If all three sides are equal, return 'Equilateral'
         If exactly one pair of sides are equal, return 'Isoceles'
         If no pair of  sides are equal, return 'Scalene'
         If not a valid triangle, then return 'NotATriangle'
         If the sum of any two sides equals the squate of the third side, then return 'Right'
-      
       BEWARE: there may be a bug or two in this code
     """
 
